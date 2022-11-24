652b702537b05f028a8a9ff82615084893948f6d
Pawandeep Sall <pawandeepk@my.com>
2022-11-24 11:37:52 +0000

    linked animation to scroll

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const flightPath = {
     curviness: 1.25,
     autoRotate: true,
     values: [
-        {x: 100, y: -20}, {x: 300, y: 10}
+        {x: 100, y: -20},
+        {x: 300, y: 10},
+        {x: 500, y: 100},
+        {x: 750, y: -100},
+        {x: 350, y: -50},
+        {x: 600, y: 100},
+        {x: 800, y: 0},
+        {x: window.innerWidth, y: -250}
     ]
 };
 
@@ -14,3 +21,14 @@ tween.add(
         ease: Power1.easeInOut
     })
 );
+
+const controller = new ScrollMagic.Controller();
+
+const scene = new ScrollMagic.Scene({
+    triggerElement: '.animation',
+    duration: 1000,
+    triggerHook: 0.25
+})
+.setTween(tween)
+.addIndicators()
+.addTo(controller);
